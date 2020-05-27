9c8c6a23ece19d911dd1b4bb43542cd0a0c18474
ocasusMaximus <za.jan@seznam.cz>
2020-05-27 18:09:30 +0200

    First Commit

---
+++ src/navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (<div className="navbar navbar-light bg-dark">
+            <div className="container">
+                <h2 className="text-white">Kalkulátor výdajů v domácnosti</h2>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default Navbar
