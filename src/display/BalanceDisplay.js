b700c9d0bf254390a86b9dd653134b4c0e224279
ocasusMaximus <za.jan@seznam.cz>
2020-06-11 15:54:49 +0200

    Balance display color change

--- src/display/BalanceDisplay.js
+++ src/display/BalanceDisplay.js
@@ -1,73 +1,69 @@
 import React from 'react';
 import { BudgetConsumer } from '../store'
 
-var style = {
-    color: 'black',
-    writable: true,
-};
+// var style = {
+//     color: 'black',
+//     writable: true,
+// };
 
 
 
 
-const BalanceDisplay = () => {
-    return (
-        <BudgetConsumer>
-            {value => {
-                const totalExpenses = value.expenses.length > 0 ? (
-                    value.expenses.reduce((acc, curr) => {
-                        acc += parseInt(curr.amount);
-                        return acc;
-                    }, 0)) : 0;
+class BalanceDisplay extends React.Component {
+    state = {
+        color: ''
+    }
+    changeColor = () => {
+        console.log('jsem tu');
+        this.setState({ color: 'red' })
 
-                const leftOver = (value.budget - totalExpenses);
-                const changeColor = () => {
+    }
 
-                    if (!leftOver.toString().includes('-')) {
+    render() {
+        return (
+            <BudgetConsumer>
+                {value => {
+                    const totalExpenses = value.expenses.length > 0 ? (
+                        value.expenses.reduce((acc, curr) => {
+                            acc += parseInt(curr.amount);
+                            return acc;
+                        }, 0)) : 0;
 
-                        style = {
-                            color: 'red'
-                        }
+                    const leftOver = (value.budget - totalExpenses);
 
-                    } else {
+                    return (
+                        <div className="row">
+                            <div className="col-lg-4">
+                                <div className="card">
+                                    <div className="card-header">Rozpočet</div>
+                                    <div className="card-body">
+                                        <h5 className="text-center card-title">{value.budget}</h5>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-4">
+                                <div className="card">
+                                    <div className="card-header"> Celkové Výdaje</div>
+                                    <div className="card-body">
+                                        <h5 className="text-center card-title">{totalExpenses}</h5>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-4">
+                                <div className="card">
+                                    <div className="card-header"> Celkový Zůstatek</div>
+                                    <div className="card-body">
 
-                        style = {
-                            color: 'green'
-                        }
-
-                    }
-                }
-                return (
-                    <div className="row">
-                        <div className="col-lg-4">
-                            <div className="card">
-                                <div className="card-header">Rozpočet</div>
-                                <div className="card-body">
-                                    <h5 className="text-center card-title">{value.budget}</h5>
+                                        <h5 className="text-center card-title" style={{ color: this.state.color }} onChange={this.changeColor} >{leftOver}</h5>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-4">
-                            <div className="card">
-                                <div className="card-header"> Celkové Výdaje</div>
-                                <div className="card-body">
-                                    <h5 className="text-center card-title">{totalExpenses}</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-4">
-                            <div className="card">
-                                <div className="card-header"> Celkový Zůstatek</div>
-                                <div className="card-body">
-
-                                    <h5 className="text-center card-title" style={style} onChange={changeColor()}>{leftOver}</h5>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )
-            }}
-        </BudgetConsumer>
-    )
+                    )
+                }}
+            </BudgetConsumer>
+        )
+    }
 }
 
 export default BalanceDisplay
