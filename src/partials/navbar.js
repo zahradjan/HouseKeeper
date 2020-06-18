0b3dc95dbc59eccf950a60a12a86f89793fd7348
ocasusMaximus <za.jan@seznam.cz>
2020-06-18 15:02:33 +0200

    Favicon and icon add

--- src/partials/navbar.js
+++ src/partials/navbar.js
@@ -4,6 +4,7 @@ class Navbar extends Component {
     render() {
         return (<div className="navbar navbar-light bg-dark">
             <div className="container">
+                <img  src="calc-icon.png" alt="Calc-icon" width="35" height="40"></img>
                 <h2 className="text-white">Kalkulátor výdajů v domácnosti</h2>
             </div>
         </div>
