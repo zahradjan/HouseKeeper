a7dd944ce78b3b66ea8110495acfe59cf7cddd14
ocasusMaximus <za.jan@seznam.cz>
2020-06-18 13:05:58 +0200

    Store deleted

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BudgetProvider} from './store';
+
 
 
 ReactDOM.render(
-    <BudgetProvider>
+  
       <App/>
-    </BudgetProvider>
+  
   
 
 
